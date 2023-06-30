892805ad5ceac8628bd864a34119b65189394d76
KamilKulczyk1 <128227168+KamilKulczyk1@users.noreply.github.com>
2023-06-30 10:09:13 +0200

    Initial commit

---
+++ Cw7Zad1.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        static string FormatTime(int seconds)
+        {
+            // Utwórz obiekt DateTime z podaną liczbą sekund
+            DateTime time = new DateTime().AddSeconds(seconds);
+            // Zwróć sformatowany czas w formacie HH:mm:ss
+            return time.ToString("HH:mm:ss");
+        }
+}
+}
+}
---
+++ Cw7Zad2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double x1 = 2;
+        double y1 = 2;
+        double x2 = 6;
+        double y2 = 10;
+
+        Console.WriteLine(DlugoscOdcinka(x1, y1, x2, y2));
+    }
+
+    public static double DlugoscOdcinka(double x1, double y1, double x2, double y2)
+    {
+        return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    }
+}
+
